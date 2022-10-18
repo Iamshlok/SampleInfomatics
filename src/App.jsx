c21aa4be3ac072b61a3ce806c017cd760431cc5c
Iamshlok <112081033+Iamshlok@users.noreply.github.com>
2022-10-18 17:19:27 +0530

    dev: Created new components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import About from "./pages/About"
+import './App.css'
+const App = () => {
+  return (
+    <div className='body'>
+      <About/>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../components/Footer/Footer'
+import LeaderTestimony from '../components/LeadCard/LeaderTestimony'
+
+const About = () => {
+  return (
+    <div>
+      <LeaderTestimony/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default About
